84bb2f63c2e51e69a01813dbd4ecee53a75cffcd
randolrs <r.shane.randolph@gmail.com>
2016-11-26 15:36:53 -0800

    red

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -1,5 +1,10 @@
 class Performer < ActiveRecord::Base
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	
+	has_attached_file :image, 
+	:styles => { :medium => "194x194#", :small => "70x70#", :thumb => "30x30#"},
+	:default_url => 'missing_person_photo.png',
+	:s3_protocol => :https
+
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
