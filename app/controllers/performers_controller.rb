f7e8e387e1d0945b6db80b0fc2f2177d4bb1e92f
randolrs <r.shane.randolph@gmail.com>
2016-11-27 16:05:48 -0800

    i got hac

--- app/controllers/performers_controller.rb
+++ app/controllers/performers_controller.rb
@@ -69,6 +69,6 @@ class PerformersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def performer_params
-      params.require(:performer).permit(:name, :bio, :city, :country, :twitter_url, :facebook_url, :instagram_url, :youtube_url, :video_url, :image)
+      params.require(:performer).permit(:name, :bio, :city, :country, :twitter_url, :facebook_url, :instagram_url, :youtube_url, :snapchat_url, :website_url, :video_url, :image)
     end
 end
