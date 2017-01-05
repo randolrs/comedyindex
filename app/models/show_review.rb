6d5280471082a2ce82ac51ea5da1b9628dee926e
randolrs <r.shane.randolph@gmail.com>
2017-01-04 17:43:23 -0800

    start review index

--- app/models/show_review.rb
+++ app/models/show_review.rb
@@ -4,6 +4,63 @@ class ShowReview < ActiveRecord::Base
 	belongs_to :show_occurence
 	belongs_to :user
 
-	
+	def css_rating_code
+		
+		rating = self.rating
+
+		if rating 
+
+			if rating == 0
+
+				return "stars-0"
+
+			elsif (0.01..1.0).include?(rating)
+
+				return "stars-1"
+
+			elsif (1.01..1.5).include?(rating)
+
+				return "stars-1-and-half"
+
+			elsif (1.51..2.0).include?(rating)
+
+				return "stars-2"
+
+			elsif (2.01..2.5).include?(rating)
+
+				return "stars-2-and-half"
+
+			elsif (2.51..3.0).include?(rating)
+
+				return "stars-3"
+
+			elsif (3.01..3.5).include?(rating)
+
+				return "stars-3-and-half"
+
+			elsif (3.51..4.0).include?(rating)
+
+				return "stars-4"
+
+			elsif (4.01..4.5).include?(rating)
+
+				return "stars-4-and-half"
+
+			elsif (4.51..5.0).include?(rating)
+
+				return "stars-5"
+
+			else
+
+				return "stars-0"
+
+			end
+		else
+
+			return "stars-0"
+
+		end
+
+	end
 
 end
