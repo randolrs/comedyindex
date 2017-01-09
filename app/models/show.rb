3a966d17d030b9b407b55bad4f69ba132a2a9b84
randolrs <r.shane.randolph@gmail.com>
2017-01-09 15:16:05 -0800

    add names, some styling

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
 
 	has_attached_file :image, 
-	:styles => { :medium => "400x400#", :small => "70x70#", :thumb => "30x30#"},
+	:styles => { :medium => "600x200#", :small => "70x70#", :thumb => "30x30#"},
 	:default_url => 'missing_person_photo.png',
 	:s3_protocol => :https
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105004835) do
+ActiveRecord::Schema.define(version: 20170109230802) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -122,6 +122,9 @@ ActiveRecord::Schema.define(version: 20170105004835) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "display_name"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
