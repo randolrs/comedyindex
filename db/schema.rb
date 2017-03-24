79d52babca789b43a6bfffd6c4fbf3b892995242
randolrs <r.shane.randolph@gmail.com>
2017-03-23 19:05:40 -0700

    okokko

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323233818) do
+ActiveRecord::Schema.define(version: 20170324015232) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20170323233818) do
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "time_zone"
   end
 
   create_table "users", force: :cascade do |t|
