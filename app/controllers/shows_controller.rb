3b4956a6f40be5f486e0f01492ac99f07719a83d
randolrs <r.shane.randolph@gmail.com>
2017-04-10 14:15:00 -0700

    fictional

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -202,7 +202,7 @@ class ShowsController < ApplicationController
           ShowOccurrence.where(:schedulable_id => @show.id).update_all(:address => @show.address, :latitude => @show.latitude, :longitude => @show.longitude)
           
 
-          format.html { redirect_to show_with_url_path(@show.url_slug), notice: 'Show was successfully created.' }
+          format.html { redirect_to show_with_url_path(@show.city, @show.url_slug), notice: 'Show was successfully created.' }
           format.json { render :show, status: :created, location: @show }
         else
           format.html { render :new }
